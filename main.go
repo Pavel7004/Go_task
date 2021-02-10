15dc180c2fc46f28cf4676703966ff751b2879ee
Kovalev Pavel <kovalev5690@gmail.com>
2021-02-10 21:13:49 +0300

    Refactor main.go

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+func createMultiInterval(a uint64, b uint64, count uint64) [][2]uint64 {
+	interval := (b-a+1)/count - 1
+	balance := (b - a + 1) % count
+	var ret [][2]uint64 = make([][2]uint64, int(count))
+	first := a
+	second := a + interval
+	for i := 0; i < int(count); i++ {
+		ret[i][0] = first
+		ret[i][1] = second
+		first = second + 1
+		second += interval + 1
+		if balance > 0 {
+			second++
+			balance--
+		}
+	}
+	return ret
+}
 
 func rangeSum(a uint64, b uint64) uint64 {
 	var sum uint64 = 0
@@ -13,34 +33,27 @@ func rangeSum(a uint64, b uint64) uint64 {
 	return sum
 }
 
+func rangeSumAsync(a uint64, b uint64) uint64 {
+	const threadCount = 4
+	intervals := createMultiInterval(a, b, threadCount)
+	var wg sync.WaitGroup
+	var sum uint64 = 0
+	wg.Add(threadCount)
+	for i := 0; i < threadCount; i++ {
+		go func(id int) {
+			sum += rangeSum(intervals[id][0], intervals[id][1])
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return sum
+}
+
 func execAndCountRuntime(a uint64, b uint64, f func(uint64, uint64) uint64) (uint64, int64) {
 	t1 := time.Now()
 	ret := f(a, b)
 	t2 := time.Now()
 	return ret, (t2.Sub(t1)).Nanoseconds()
-}
-
-func rangeSumAsyncWorker(ch chan uint64, a uint64, b uint64) {
-	ch <- rangeSum(a, b)
-}
-
-func rangeSumAsync(a uint64, b uint64) uint64 {
-	var sum uint64 = 0
-	interval := (b-a+1)/4 - 1
-	ch := make(chan uint64, 3)
-	firstArg, secondArg := a, a+interval
-	for i := 0; i < 4; i++ {
-		go rangeSumAsyncWorker(ch, firstArg, secondArg)
-		firstArg = secondArg + 1
-		secondArg = firstArg + interval
-		if i == 2 {
-			secondArg = b
-		}
-	}
-	for i := 0; i < 4; i++ {
-		sum += (<-ch)
-	}
-	return sum
 }
 
 func main() {
