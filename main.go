9d3e846a2485e72dc4b6da18249a20045b7e247c
Kovalev Pavel <kovalev5690@gmail.com>
2021-02-11 14:35:18 +0300

    Removed function createMultiInterval

--- main.go
+++ main.go
@@ -6,25 +6,6 @@ import (
 	"time"
 )
 
-func createMultiInterval(a uint64, b uint64, count uint64) [][2]uint64 {
-	interval := (b-a+1)/count - 1
-	balance := (b - a + 1) % count
-	var ret [][2]uint64 = make([][2]uint64, int(count))
-	first := a
-	second := a + interval
-	for i := 0; i < int(count); i++ {
-		ret[i][0] = first
-		ret[i][1] = second
-		first = second + 1
-		second += interval + 1
-		if balance > 0 {
-			second++
-			balance--
-		}
-	}
-	return ret
-}
-
 func rangeSum(a uint64, b uint64) uint64 {
 	var sum uint64 = 0
 	for i := a; i <= b; i++ {
@@ -35,15 +16,24 @@ func rangeSum(a uint64, b uint64) uint64 {
 
 func rangeSumAsync(a uint64, b uint64) uint64 {
 	const threadCount = 4
-	intervals := createMultiInterval(a, b, threadCount)
+	interval := (b-a+1)/threadCount - 1
+	balance := (b - a + 1) % threadCount
 	var wg sync.WaitGroup
 	var sum uint64 = 0
+	first := a
+	second := first + interval
 	wg.Add(threadCount)
 	for i := 0; i < threadCount; i++ {
-		go func(id int) {
-			sum += rangeSum(intervals[id][0], intervals[id][1])
+		go func(a uint64, b uint64) {
+			sum += rangeSum(a, b)
 			wg.Done()
-		}(i)
+		}(first, second)
+		first = second + 1
+		second += interval + 1
+		if balance > 0 {
+			second++
+			balance--
+		}
 	}
 	wg.Wait()
 	return sum
